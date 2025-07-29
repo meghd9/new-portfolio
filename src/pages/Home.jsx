1ea5019b7782effe9ce939ce12e2ced0ce234e45
Megh <meghd9@gmail.com>
2025-07-29 15:54:57 +0100

    Completed Hero section, worked on Navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,6 @@
+import { AboutSection } from "../components/AboutSection";
+import { HeroSection } from "../components/HeroSection";
+import { Navbar } from "../components/Navbar";
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
@@ -6,6 +9,9 @@ const Home = () => {
     <div>
       <ThemeToggle />
       <StarBackground />
+      <Navbar />
+      <HeroSection />
+      <AboutSection />
     </div>
   );
 };
